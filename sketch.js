f54d764bea095121a9fd4c6c8c4d0bb9668b875c
Aaryansajit <66948471+Aaryansajit@users.noreply.github.com>
2020-08-20 15:40:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -59,6 +59,6 @@ function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
 	Matter.Body.setStatic(packageBody, false)
-	packageBody.restitution = 0.8
+	packageBody.restitution = 0.65
   }
 }
